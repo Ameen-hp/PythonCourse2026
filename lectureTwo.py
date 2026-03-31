4afd988fb857766e99d37efa3730491ce8941d31
Ameen-hp <devameen45@gmail.com>
2026-03-31 22:27:18 +0500

    LectureTwo ... feat(python-basics): add introduction to variables, naming rules, data types, and operators
    - Explained variables as data containers with examples
    - Added variable naming rules (valid/invalid cases)
    - Covered naming conventions (camelCase, PascalCase, snake_case)
    - Introduced basic data types (string, int, float)
    - Demonstrated arithmetic operators (+, -, *, /, %, **)
    - Included simple print examples for better understanding

---
+++ lectureTwo.py
@@ -0,0 +1,85 @@
+
+# Variables 
+# are containers those are used for storing data like x,y 
+
+w = 12
+name="Tanzeel"
+print(name,"my name is ameen")
+
+
+# Variable Rules
+
+# Rule 1
+# A variable name must start with a letter or the underscore character
+store="hospital"     #correct
+_color="red"         #correct
+
+#Rule 2
+# A variable name cannot start with a number
+# 123Address    #wrong
+
+#Rule 3
+# A variable name can only contain alpha-numeric characters and underscores (A-z
+# , 0-9, and _ )
+
+# Address123="Karachi" #correct
+# Adress+123 = ""
+# Adress_1234=98
+
+#Rule 4
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+# age=123;
+# Age=89;
+
+#Rule 5
+# A variable name cannot be any of the Python keywords.
+
+# print = 2321
+
+# print(print);
+
+
+
+
+# camel case 
+# first letter of varaible should be in small case. rest in Capitals
+# myNameIs = "Ameen"
+# Pascal case 
+# All Starting letters of Words should be capital 
+# MyNameIs = "Ameen"
+# Snake case 
+# All letters should be in small case, next word should start with _
+# my_name_is = "Ameen";
+
+
+# Data Type 
+# a type of data in the variable value is data type 
+
+# Text 
+y = "Python" #String
+z = "12234Ameen()***" #String
+
+
+# Numeric Type
+# x = 12   # int
+# t = 3.45  # float 
+# u = 3+ab  # Complex
+
+
+# Operetors
+# join 2 or more operends toghether and produce a result 
+# Example
+# operand =>2+operetor 2=> operend 
+# Arithematic Operetors
+# +,-,*,/,%,**
+
+x = 2;
+y = 4
+print("result of y+z= ",y+x)
+print("result of y-z= ",y-x)
+print("result of y*z= ",y*x)
+print("result of y/z= ",y/x)
+print("result of y%z= ",y%x)
+print("result of y**z= ",y**3)
+
