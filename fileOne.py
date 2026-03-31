49de508f21258cc13a87dba23c7372715e21eefa
Ameen-hp <devameen45@gmail.com>
2026-03-31 20:50:36 +0500

    Feat: introduction to computer, programming languages, python basics

---
+++ fileOne.py
@@ -0,0 +1,14 @@
+
+# print is a function in python which is used to display the output on the console. It can take multiple arguments and can be used to print different types of data such as strings, numbers, lists, etc.
+
+# Indentation
+# indentation is a process when we provide space to our code then code provide error 
+
+print("we are Learning python");
+print('we are Learning python');
+
+#triple qoutation
+# used for printing in multiline and same formate that is in the vs code 
+
+print(""" we are 
+      Learning python""")
