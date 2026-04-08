0ba505eb669e0ee3de446363b128e1cb3769a3c5
Ameen-hp <devameen45@gmail.com>
2026-04-09 00:32:24 +0500

    feat: teach about % and nested if with examples

---
+++ lecture5.py
@@ -0,0 +1,62 @@
+
+
+# % ==>  0 , 1,2 
+
+# numOne  = 12;
+# numTwo = 10;
+# print(numOne%numTwo)
+
+# if we are to check an number for odd or even ...  number%2 === 0 ... even, odd
+
+
+check_num = -1  # infite
+
+# check the number is odd or even 
+# if(check_num%2 == 0):
+#      print("the check_num is even");
+
+# elif(check_num<0):
+#      print("the number is infinite")
+# else:
+#      print("the check_num is odd")
+
+
+
+# nested if 
+#  Technical Consultant (level 1)
+#  Associate Techniqcal Consultant (level 2)
+ # 
+# cgpa = 2.5;
+
+# if(cgpa > 2):
+#    print("you are eligble for job")
+#    if(cgpa == 3):
+#       print(" Technical consultant (level 1)")
+#    elif(cgpa > 3.5 or cgpa == 4 ):
+    
+#       print("Associate Technical Consultant (level 2)")
+  
+      
+# else:
+#    print("you are not eligible for the job")   
+
+
+   
+
+
+
+hours = 3
+
+if(hours >= 1):
+    print("you are to complete the one task")
+    if(hours == 1.5  or hours < 3):
+        print("ypu are to compelete second task")
+    elif(hours == 3):
+        print("complete third task")    
+else:
+    print("you are to at least clear the concept of task ")        
+
+
+
+
+
