954531463f5003f20e7dd9c9b11f4fb1f259ba76
Ameen-hp <devameen45@gmail.com>
2026-04-14 22:49:35 +0500

    docs: add beginner-friendly Python loop practice examples
    - Basic for loop with range
    - Range with start and end values
    - Loop control using break condition
    - Step increment using third parameter
    - User input based multiplication table example

---
+++ lecture7.py
@@ -0,0 +1,42 @@
+
+#  loop=>  
+
+#  range = 4 , x=0
+#   
+
+# for x in  range(4):
+#     print(x,"my name is ameen") # 0,x++=x=x+1,x=1,1,x=x+1,x=2,2,3,  
+
+# using second parameter in range 
+
+# for x in range(4,20):
+#     print("the value of x is ",x)
+
+
+
+# using continue key word with for loop
+
+# for x in range(10):
+     
+#      if(x==5):
+#           break
+#      else:
+#           print(x)
+
+
+
+# using third parameter with for loop 
+# to increment in every iteration 
+
+# first parameter = initialization 
+# second parameter = define the range or we can say the condition
+# thir parameter = increment 
+# for x in range(1,20,2):
+#     print(x)
+
+
+# ending_point  = int(input("please enter the range of the table  "))
+# starting_poin = int(input("please enter the starting point of table")) 
+# table = int(input("please enter the table"))
+# for x in range(starting_poin,ending_point):
+#     print(x ,'*' , table, '=',x*table)
